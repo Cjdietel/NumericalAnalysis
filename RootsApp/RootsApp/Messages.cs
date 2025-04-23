496b27ab7a879eb04221c4e9072b7c111c3f23eb
Cjdietel <114264388+Cjdietel@users.noreply.github.com>
2025-04-23 07:23:48 -0400

    Add files via upload

---
+++ RootsApp/RootsApp/Messages.cs
@@ -0,0 +1,40 @@
+﻿namespace RootsApp
+{
+    
+    public class Messages
+    {
+
+        public enum ROOT_ERRORS { NONE, SAME_SIGN, DIVISION_BY_ZERO, RUNAWAY, CYCLE };
+        public static void DivisionByZeroError(RootObj r)
+        {
+            
+            r.root = double.NaN;
+            r.error = (int)ROOT_ERRORS.DIVISION_BY_ZERO;
+            
+        }
+
+        public static void SameSign(RootObj r)
+        {
+
+            r.root = double.NaN;
+            r.error = (int)ROOT_ERRORS.SAME_SIGN;
+
+        }
+
+        public static void RunawayError(RootObj r)
+        {
+            
+            r.root = double.NaN;
+            r.error = (int)ROOT_ERRORS.RUNAWAY;
+            
+        }
+
+        public static void CycleError(RootObj r)
+        {
+           
+            r.root = double.NaN;
+            r.error = (int)ROOT_ERRORS.CYCLE;
+            
+        }
+    }
+}
